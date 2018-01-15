bd3579872c38b0b31fadd02c28cd4a63c7a22b04
啥玩意儿啊 <120920774@qq.com>
2018-01-15 22:27:48 +0800

    纯代码自动布局NSLayoutConstraint

--- Weibo/Weibo/Classes/View/Main/VisitorView/WBVisitorView.swift
+++ Weibo/Weibo/Classes/View/Main/VisitorView/WBVisitorView.swift
@@ -19,6 +19,23 @@ class WBVisitorView: UIView {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    //MARK:添加私有控件
+    //懒加载属性只有调用 UIKit 控件的指定的构造函数,其他都需要使用类型
+    //图像视图
+    private lazy var iconView:UIImageView = UIImageView(image:UIImage(named:"visitordiscover_feed_image_smallicon"))
+    //小房子
+    private lazy var houseIconView:UIImageView = UIImageView(image:UIImage(named:"visitordiscover_feed_image_house"))
+    
+    //提示标签
+    private lazy var tipLabel:UILabel = UILabel.cz_label(withText: "关注一些人,回这里看看有什么惊喜", fontSize: 14, color: UIColor.darkGray)
+    
+    //注册按钮
+    private lazy var registerButton:UIButton = UIButton.cz_textButton("注册", fontSize: 16, normalColor: UIColor.orange, highlightedColor: UIColor.black, backgroundImageName: "common_button_white_disable")
+    
+    //登陆按钮
+    private lazy var loginButton:UIButton = UIButton.cz_textButton("登录", fontSize: 16, normalColor: UIColor.darkGray, highlightedColor: UIColor.black, backgroundImageName: "common_button_white_disable")
+    
 
 }
 
@@ -28,7 +45,47 @@ extension WBVisitorView {
     func setupUI(){
         
         self.backgroundColor = UIColor.white
+        //1.添加控件
+        addSubview(iconView)
+        addSubview(houseIconView)
+        addSubview(tipLabel)
+        addSubview(registerButton)
+        addSubview(loginButton)
         
+        for v in subviews {
+            v.translatesAutoresizingMaskIntoConstraints = false
+        }
+        //2.取消autoresing
+        
+        addConstraint(NSLayoutConstraint.init(item: iconView,
+                                              attribute: .centerX,
+                                              relatedBy: .equal,
+                                              toItem: self,
+                                              attribute: .centerX,
+                                              multiplier: 1.0,
+                                              constant: 0))//中心x
+        addConstraint(NSLayoutConstraint.init(item: iconView,
+                                              attribute: .centerY,
+                                              relatedBy: .equal,
+                                              toItem: self,
+                                              attribute: .centerY,
+                                              multiplier: 1.0,
+                                              constant: 0))
+        //小房子
+        addConstraint(NSLayoutConstraint.init(item: houseIconView,
+                                              attribute: .centerX,
+                                              relatedBy: .equal,
+                                              toItem: iconView,
+                                              attribute: .centerX,
+                                              multiplier: 1.0,
+                                              constant: 0))//中心x
+        addConstraint(NSLayoutConstraint.init(item: houseIconView,
+                                              attribute: .centerY,
+                                              relatedBy: .equal,
+                                              toItem: iconView,
+                                              attribute: .centerY,
+                                              multiplier: 1.0,
+                                              constant: 0))
         
     }
 
