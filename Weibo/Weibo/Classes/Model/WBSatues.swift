0d1a843940e1dc717c5f17c09d085e0ab289529f
啥玩意儿啊 <120920774@qq.com>
2018-01-24 20:12:07 +0800

    建立微博数据模型

---
+++ Weibo/Weibo/Classes/Model/WBSatues.swift
@@ -0,0 +1,25 @@
+//
+//  WBSatues.swift
+//  Weibo
+//
+//  Created by ZhangXu on 2018/1/24.
+//  Copyright © 2018年 张旭. All rights reserved.
+//
+
+import UIKit
+import YYModel
+//微博数据模型
+class WBSatues: NSObject {
+    
+    //Int类型在64位机型是64的 32位是32位 若不写64 在老旧机型上都无法正常运行
+    var id:Int64 = 0
+    //微博信息内容
+    var text:String?
+    
+    //重写description的计算型属性
+    override var description: String{
+        
+        return yy_modelDescription()
+        
+    }
+}
