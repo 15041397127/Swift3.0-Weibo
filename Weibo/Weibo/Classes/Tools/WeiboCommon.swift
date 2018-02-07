776a05dfda77d1cc517a5cf69a0dd3f6a2abdfbe
ZhangXu <zhangxu@ZXdeMacBook-Pro.local>
2018-02-07 13:54:38 +0800

    添加应用程序信息

--- Weibo/Weibo/Classes/Tools/WeiboCommon.swift
+++ Weibo/Weibo/Classes/Tools/WeiboCommon.swift
@@ -7,6 +7,14 @@
 //
 
 import Foundation
+
+//MARK: - 应用程序信息
+let WBAppKey =   "1012589697"
+let WBAppSecret =   "4f08aa9a4beff9338d27d365e8c63af0"
+let WBRedirectURI =   "https://www.baidu.com" //回调地址
+
+//https://api.weibo.com/oauth2/authorize?client_id=1012589697&redirect_uri=https://www.baidu.com
+
 //MARK:全局通知定义
 
 let WBUserShouldLoginNotification = "WBUserShouldLoginNotification"
