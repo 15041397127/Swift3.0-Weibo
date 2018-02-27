927a7b99ee9734098ff5e6b85ce6daa352043cd2
啥玩意儿啊 <120920774@qq.com>
2018-02-27 20:57:05 +0800

    欢迎 新特性视图

---
+++ Weibo/Weibo/Classes/View/Main/NewFeature/WBNewFeature.swift
@@ -0,0 +1,31 @@
+//
+//  WBNewFeature.swift
+//  Weibo
+//
+//  Created by ZhangXu on 2018/2/27.
+//  Copyright © 2018年 张旭. All rights reserved.
+//
+
+import UIKit
+
+//新特性视图
+class WBNewFeature: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = UIColor.yellow
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Weibo/Weibo/Classes/View/Main/NewFeature/WBWelcomeView.swift
@@ -0,0 +1,32 @@
+//
+//  WBWelcomeView.swift
+//  Weibo
+//
+//  Created by ZhangXu on 2018/2/27.
+//  Copyright © 2018年 张旭. All rights reserved.
+//
+
+import UIKit
+//欢迎视图
+class WBWelcomeView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = UIColor.blue
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+         fatalError("init(coder:) has not been implemented")
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
