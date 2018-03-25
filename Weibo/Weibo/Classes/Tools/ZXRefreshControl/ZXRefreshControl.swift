17508069ec9324ca48b6f5c9bc99258aed07ef28
啥玩意儿啊 <120920774@qq.com>
2018-03-25 20:18:24 +0800

    移除KVO监听

--- Weibo/Weibo/Classes/Tools/ZXRefreshControl/ZXRefreshControl.swift
+++ Weibo/Weibo/Classes/Tools/ZXRefreshControl/ZXRefreshControl.swift
@@ -20,12 +20,14 @@ class ZXRefreshControl: UIControl {
     init() {
         
         super.init(frame: CGRect())
-        
+       
         setupUI()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+         //xib也可以使用
+        setupUI()
     }
     
     override func willMove(toSuperview newSuperview: UIView?) {
@@ -45,13 +47,32 @@ class ZXRefreshControl: UIControl {
       
         scrollView = sv
         
-        //KVO监听父视图 contentOffset
+        //KVO监听父视图 contentOffset  KVO要监听的对象负责添加监听者
+        //在程序中,只监听某一个对象的某几个属性 如果属性太多 方法会很乱
         scrollView?.addObserver(self, forKeyPath: "contentOffset", options: [], context: nil)
         
         
     }
     
+    //本视图从父视图移除
+    //提示 所有的下拉刷新框架都是监听父视图的contentOffset
+    //所有框架的KVO监听实现思路都是这个
+    override func removeFromSuperview() {
+        //superView还存在
+        
+        superview?.removeObserver(self, forKeyPath: "contentOffset")
+        
+        super .removeFromSuperview()
+        
+        //superView不存在
+        
+    }
+    
+    
     //所有KVO 会调用此方法
+    //观察者模式 在不需要的时候都需要释放
+    //1.通知中心 如果不释放 什么也不会发生 但是会内存泄露 会多次注册的可能
+    //2.kvo会崩溃
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         //contentOffset 的y值 跟contentInset 的top有关
         guard let sv = scrollView else {
