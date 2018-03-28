953dd319fab7869781d09cdeff6028eea3c69373
ZhangXu <zhangxu@ZXdeMacBook-Pro.local>
2018-03-28 20:50:05 +0800

    增加房子和地球的 核心动画isRemovedOnCompletion

--- Weibo/Weibo/Classes/Tools/ZXRefreshControl/ZXMeiTuanRefreshView.swift
+++ Weibo/Weibo/Classes/Tools/ZXRefreshControl/ZXMeiTuanRefreshView.swift
@@ -13,6 +13,27 @@ class ZXMeiTuanRefreshView: CZRefreshView {
     @IBOutlet weak var buildingIconView: UIImageView!//提示图标
     @IBOutlet weak var refresh_earthIconView: UIImageView!//提示图标
     @IBOutlet weak var loadingIconView: UIImageView!//提示图标
+    
+    override func awakeFromNib() {
+        
+        //1.房子
+        let bImage1 = #imageLiteral(resourceName: "takeout_img_refresh_building_1")
+        let bImage2 = #imageLiteral(resourceName: "takeout_img_refresh_building_2")
+        
+        buildingIconView.image = UIImage.animatedImage(with: [bImage1,bImage2], duration: 0.5)
+        
+        //地球
+        let anim = CABasicAnimation(keyPath: "transform.rotation")
+        anim.toValue = -2 * M_PI
+        anim.repeatCount = MAXFLOAT
+        anim.duration = 3
+        
+        anim.isRemovedOnCompletion = false
+        
+        refresh_earthIconView.layer.add(anim, forKey: nil)
+        
+        
+    }
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
