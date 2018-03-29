614c79be5c103abd67215f6027039912c09b7859
啥玩意儿啊 <120920774@qq.com>
2018-03-29 20:08:07 +0800

    使用xib加载视图

--- Weibo/Weibo/Classes/View/Compose/ComposeType/WBComposeTypeView.swift
+++ Weibo/Weibo/Classes/View/Compose/ComposeType/WBComposeTypeView.swift
@@ -11,15 +11,27 @@ import UIKit
 //撰写微博类型视图
 class WBComposeTypeView: UIView {
 
-    override init(frame: CGRect) {
-        super.init(frame: UIScreen.main.bounds)
-        
-        backgroundColor = UIColor.cz_random()
-    }
+//    override init(frame: CGRect) {
+//        super.init(frame: UIScreen.main.bounds)
+//
+//        backgroundColor = UIColor.cz_random()
+//    }
+//
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
     
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    class func composeTypeView() -> WBComposeTypeView{
+        
+        let nib = UINib(nibName: "WBComposeTypeView", bundle: nil)
+        let v = nib.instantiate(withOwner: nil, options: nil)[0] as! WBComposeTypeView
+        
+        //XIB 加载默认是 600 * 600
+        v.frame = UIScreen.main.bounds
+        
+        return v
     }
+
       //显示当前视图
     func show(){
         
