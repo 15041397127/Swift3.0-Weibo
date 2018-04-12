e0773425a78a307b126149a284b5ebd2e83a26b4
啥玩意儿啊 <120920774@qq.com>
2018-04-12 22:29:42 +0800

    按照整页设置布局

--- Weibo/Weibo/Classes/Tools/Emoticon/View/WBEmoticonCellLayout.swift
+++ Weibo/Weibo/Classes/Tools/Emoticon/View/WBEmoticonCellLayout.swift
@@ -13,6 +13,16 @@ class WBEmoticonCellLayout: UICollectionViewFlowLayout {
     //prepare 就是OC中的prepareLayout
     override func prepare() {
         super.prepare()
+        //在此方法中 collectionView大小以及确定
+        
+        guard let collectionView = collectionView else {
+            return
+        }
+        
+        itemSize = collectionView.bounds.size
+//        minimumLineSpacing = 0//行间距
+//        minimumInteritemSpacing = 0//格子间距
+        //上面两行以及在在xib中设置
         //设定滚动方法
         //水平方向滚动  cell是垂直方向布局
         //垂直方向滚动  cell是水平方向布局
