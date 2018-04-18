c576ae71171f7c2939741066646b2da9e4c2b2db
zhangxu18 <120920774@qq.com>
2018-04-18 21:01:56 +0800

    新增数据访问层 确定数据缓存思路

---
+++ Weibo/Weibo/Classes/ViewModel/WBStatusListDAL.swift
@@ -0,0 +1,18 @@
+//
+//  WBStatusListDAL.swift
+//  Weibo
+//
+//  Created by ZhangXu on 2018/4/18.
+//  Copyright © 2018年 张旭. All rights reserved.
+//
+
+import Foundation
+//DAL - Data Access Layer 数据访问层
+//使命 负责处理数据库和网络层 给ListViewModel 返回微博的字典数组
+//在调整系统的时候 尽量做最小化的调整
+class WBStatusListDAL {
+    
+    
+    
+    
+}
