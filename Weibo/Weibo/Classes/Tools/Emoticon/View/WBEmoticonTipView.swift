2e4c415b58ad029152aeb421227c94ba3ee3d66e
zhangxu18 <120920774@qq.com>
2018-04-19 20:53:18 +0800

    通过定位点 设置提示按钮的位置

--- Weibo/Weibo/Classes/Tools/Emoticon/View/WBEmoticonTipView.swift
+++ Weibo/Weibo/Classes/Tools/Emoticon/View/WBEmoticonTipView.swift
@@ -10,6 +10,10 @@ import UIKit
 //表情选中提示视图
 class WBEmoticonTipView: UIImageView {
 
+    //MARK:私有控件
+    private lazy var tipButton = UIButton()
+    
+    //MARK:构造函数
     init() {
         let bundle = WBEmoticonManager.shared.bundle
         let image = UIImage(named: "emoticon_keyboard_magnifier", in: bundle, compatibleWith: nil)
@@ -18,6 +22,15 @@ class WBEmoticonTipView: UIImageView {
         
         //设置锚点
         layer.anchorPoint = CGPoint(x: 0.5, y: 1.2)
+        
+        //添加按钮
+        tipButton.layer.anchorPoint = CGPoint(x: 0.5, y: 0)
+        tipButton.frame = CGRect(x: 0, y: 8, width: 36, height: 36)
+        tipButton.center.x = bounds.width * 0.5
+        tipButton.setTitle("😆", for: [])
+        tipButton.titleLabel?.font = UIFont.systemFont(ofSize: 32)
+        addSubview(tipButton)
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
